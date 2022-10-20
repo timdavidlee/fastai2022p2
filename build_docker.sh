68e2091ac1c7b693838f889d259e93868995bd44
timdavidlee <tim.david.lee@gmail.com>
2022-10-19 23:19:35 -0700

    updating changes

--- build_docker.sh
+++ build_docker.sh
@@ -1,3 +1,7 @@
+echo "cleaning up any dangling images"
+docker system prune -f
+
+echo "if the docker image was built successfully you should see a new entry:"
 docker build --tag=fastai-gpu-jlab:local -f ./docker/Dockerfile .
 
 echo "if the docker image was built successfully you should see a new entry:"
