c3073f010e95937993100ecf303585b9b51eefa1
timdavidlee <tim.david.lee@gmail.com>
2022-10-19 22:53:08 -0700

    moved files to more logical locations

---
+++ docker/gpu_check_pytorch.py
@@ -0,0 +1,13 @@
+import torch
+
+
+def main():
+    cuda_is_avail = torch.cuda.is_available()
+    print(f"CUDA is available {cuda_is_avail}")
+
+    device_name = torch.cuda.get_device_name(0)
+    print(f"CUDA device found: {device_name}")
+
+
+if __name__ == "__main__":
+    main()
