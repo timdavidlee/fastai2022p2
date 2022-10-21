5b417793a26e0c005ee12b04d0f052d49ac25c45
timdavidlee <tim.david.lee@gmail.com>
2022-10-21 00:34:10 -0700

    updates

--- setup_scripts/lambda_gpus/bootstrap.sh
+++ setup_scripts/lambda_gpus/bootstrap.sh
@@ -45,3 +45,22 @@ mkdir -p ~/ml_lessons
 cd ~/ml_lessons
 git clone https://github.com/fastai/course22p2
 git clone https://github.com/fastai/diffusion-nbs
+
+
+echo -e "${yellow}: installing nvidia-docker2"
+# https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#installation-guide
+distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
+      && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
+         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+# update and install
+sudo apt-get update
+sudo apt-get install -y nvidia-docker2
+
+echo -e "${yellow}: restarting docker"
+sudo systemctl restart docker
+
+echo -e "${yellow}: testing with a simple container"
+sudo docker run --rm --gpus all nvidia/cuda:11.0.3-base-ubuntu20.04 nvidia-smi
