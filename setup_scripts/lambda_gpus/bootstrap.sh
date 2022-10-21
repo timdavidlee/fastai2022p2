fffd50e6b443838b617a4f08863c503189bdc88b
timdavidlee <tim.david.lee@gmail.com>
2022-10-21 00:44:43 -0700

    updating versions

--- setup_scripts/lambda_gpus/bootstrap.sh
+++ setup_scripts/lambda_gpus/bootstrap.sh
@@ -51,9 +51,9 @@ echo -e "${yellow}: installing nvidia-docker2"
 # https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/install-guide.html#installation-guide
 distribution=$(. /etc/os-release;echo $ID$VERSION_ID) \
       && curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
-      && curl -s -L https://nvidia.github.io/libnvidia-container/experimental/$distribution/libnvidia-container.list | \
-         sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
-         sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+      && curl -s -L https://nvidia.github.io/libnvidia-container/$distribution/libnvidia-container.list | \
+            sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+            sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
 
 # update and install
 sudo apt-get update
